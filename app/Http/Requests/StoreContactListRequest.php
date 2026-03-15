00490e2719a95b0b77fcf7a868cc0db42c202284
Rui2020pro <matosrui1998@gmail.com>
2026-03-15 10:50:19 +0000

    add/change form request validation for contacts and contact lists

--- app/Http/Requests/StoreContactListRequest.php
+++ app/Http/Requests/StoreContactListRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreContactRequest extends FormRequest
+class StoreContactListRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,7 @@ class StoreContactRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name'   => ['required', 'string', 'max:255'],
-            'email'  => ['required', 'email', 'unique:contacts,email'],
-            'status' => ['sometimes', 'in:active,unsubscribed'],
+            'name' => ['required', 'string', 'max:255'],
         ];
     }
 }
